0b0975c03b3c809288892bd229032c433d878613
ClaudianiCerino <155901794+ClaudianiCerino@users.noreply.github.com>
2024-02-19 18:35:32 -0300

    notes-components
    Cria componentes das notas

--- nlw-expert/src/app.tsx
+++ nlw-expert/src/app.tsx
@@ -1,7 +1,15 @@
 import logo from './assets/logo-nlw-expert.svg'
+import { NewNoteCard } from './components/new-note-card';
+import { NoteCard } from './components/note-card';
+
+const note = {
+  date: new Date(),
+  content: "hellow"
+
+}
 
 export function App() {
-  return ( 
+  return (
     <div className="mx-auto max-w-6xl my-12 space-y-6">
       <img src={logo} alt="NLW Expert" />
 
@@ -16,60 +24,13 @@ export function App() {
       <div className="h-px bg-slate-700" />
 
       <div className="grid grid-cols-3 gap-6 auto-rows-[250px]">
-        <div className="rounded-md bg-slate-700 p-5 space-y-3">
-          <span className="text-sm font-medium text-slate-200">
-            {" "}
-            Adicionar nota{" "}
-          </span>
-          <p className="text-sm leading-6 text-slate-400">
-            Grave uma nota em áudio que será convertida em texto
-            automaticamente.{" "} 
-          </p>
-        </div>
-
-        <div className="rounded-md text-left bg-slate-800 p-5 space-y-3 overflow-hidden relative hover:ring-2 hover:ring-slate-600 focus-visible:ring-2 focus-visible:ring-lime-400 outline-none">
-          <span className="text-sm font-medium text-slate-300">há 2 dias</span>
-
-          <p className="text-sm leading-6 text-slate-400">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus
-            commodi dolor quaerat similique asperiores minus vitae magni nemo
-            animi iusto ducimus sapiente, error placeat ratione vel.
-            Voluptatibus sequi quis ratione. Lorem ipsum dolor sit amet
-            consectetur adipisicing elit. Natus commodi dolor quaerat similique
-            asperiores minus vitae magni nemo animi iusto ducimus sapiente,
-            error placeat ratione vel. Voluptatibus sequi quis ratione.
-          </p>
-        </div>
-
-        <div className="rounded-md text-left bg-slate-800 p-5 space-y-3 overflow-hidden relative hover:ring-2 hover:ring-slate-600 focus-visible:ring-2 focus-visible:ring-lime-400 outline-none">
-          <span className="text-sm font-medium text-slate-300">há 3 dias</span>
-
-          <p className="text-sm leading-6 text-slate-400">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus
-            commodi dolor quaerat similique asperiores minus vitae magni nemo
-            animi iusto ducimus sapiente, error placeat ratione vel.
-            Voluptatibus sequi quis ratione. Lorem ipsum dolor sit amet
-            consectetur adipisicing elit. Natus commodi dolor quaerat similique
-            asperiores minus vitae magni nemo animi iusto ducimus sapiente,
-            error placeat ratione vel. Voluptatibus sequi quis ratione.
-          </p>
-        </div>
+        <NewNoteCard/>
+        <NoteCard date={new Date()} content='Hellow' />
         
-        <div className="rounded-md text-left bg-slate-800 p-5 space-y-3 overflow-hidden relative hover:ring-2 hover:ring-slate-600 focus-visible:ring-2 focus-visible:ring-lime-400 outline-none">
-          <span className="text-sm font-medium text-slate-300">há 4 dias</span>
 
-          <p className="text-sm leading-6 text-slate-400">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus
-            commodi dolor quaerat similique asperiores minus vitae magni nemo
-            animi iusto ducimus sapiente, error placeat ratione vel.
-            Voluptatibus sequi quis ratione. Lorem ipsum dolor sit amet
-            consectetur adipisicing elit. Natus commodi dolor quaerat similique
-            asperiores minus vitae magni nemo animi iusto ducimus sapiente,
-            error placeat ratione vel. Voluptatibus sequi quis ratione.
-          </p>
-        </div>
 
-        
+
+
       </div>
     </div>
   );
