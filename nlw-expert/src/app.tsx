ed0fbf62134669dbf4e9c5bb9774221e841eacbb
ClaudianiCerino <155901794+ClaudianiCerino@users.noreply.github.com>
2024-03-09 13:00:03 -0300

    Responsive
    Ajusta responsividade do projeto

--- nlw-expert/src/app.tsx
+++ nlw-expert/src/app.tsx
@@ -63,7 +63,7 @@ export function App() {
 
 
 return (
-  <div className="mx-auto max-w-6xl my-12 space-y-6 px-5">
+  <div className="mx-auto max-w-6xl my-12 space-y-6 px-5 md:px-0">
     <img src={logo} alt="NLW Expert" />
 
     <form className="w-full">
